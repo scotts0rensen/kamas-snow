f96b02cb306839ceaf9e4b404d3ac70cef784357
Scott Sorensen <scott.sorensen@octanner.com>
2014-05-04 01:57:38 -0600

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KamasSnow::Application.config.secret_key_base = '10e616c07c0345b44c41f46adfb8f4ccbb913e22bcf79c7713fbb8817804c490051a639c6c3de643b5378c29d92732f2c0fa9fdbca0d8347af1d73974b6fff7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KamasSnow::Application.config.session_store :cookie_store, key: '_kamas-snow_session'
