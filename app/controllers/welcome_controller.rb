2411e89242460a17a0f7d0b37c8408bb0f32900c
Scott Sorensen <scott.sorensen@octanner.com>
2015-03-22 23:10:28 -0600

    add confirmation page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def apply
   end
+
+  def submit
+    redirect_to confirm_path
+  end
+
+  def confirm
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ KamasSnow::Application.routes.draw do
   root 'welcome#index'
 
   get 'application' => 'welcome#apply'
+  post 'submit' => 'welcome#submit'
+  get 'confirm' => 'welcome#confirm'
 end
